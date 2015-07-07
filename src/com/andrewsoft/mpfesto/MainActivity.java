db67b9739ebede25ec874bcb45758add9d455b5b
jakabandras <jakabandras210@gmail.com>
2015-07-07 11:35:53 +0200

    a

--- src/com/andrewsoft/mpfesto/MainActivity.java
+++ src/com/andrewsoft/mpfesto/MainActivity.java
@@ -95,7 +95,7 @@ public class MainActivity extends Activity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
